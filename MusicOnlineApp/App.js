19d1777e306ba7eb2d99b852146c8ad6cf519b89
Văn Bảo <92735734+Gun2717@users.noreply.github.com>
2024-11-10 21:04:42 +0700

    Văn Bảo setup
    Văn Bảo setup

---
+++ MusicOnlineApp/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { SideMenu } from './src/Components/Router';
+
+export default class App extends Component {
+    render() {
+        return (
+            <SideMenu />
+        );
+    }
+}
